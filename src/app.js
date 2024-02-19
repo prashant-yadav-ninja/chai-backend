b8439b29e0662b66450471b569ac908006da8033
Prashant Yadav <prashanty20720@gmail.com>
2024-02-19 15:11:01 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -11,4 +11,11 @@ app.use(express.json({limit: "16kb",}));
 app.use(express.urlencoded({limit: "16kb",extended:true}));
 
 
+// router import 
+
+import userRouter from "./routes/user.routes.js"
+
+app.use("/api/users",userRouter)
+
+
 export { app };
